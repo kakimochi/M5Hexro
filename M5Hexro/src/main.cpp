bfe1b4dc2fe6fb95ae03a9c5520979efd36942a2
kmokmops <94213372+kmokmops@users.noreply.github.com>
2024-04-07 23:14:13 +0900

    - Fix : timer_motion interrupt

--- M5Hexro/src/main.cpp
+++ M5Hexro/src/main.cpp
@@ -2,25 +2,68 @@
 
 // PA HUB
 #include <Wire.h>
-#include <SparkFun_I2C_Mux_Arduino_Library.h> // http://librarymanager/All#SparkFun_I2C_Mux https://github.com/sparkfun/SparkFun_I2C_Mux_Arduino_Library
+#include <SparkFun_I2C_Mux_Arduino_Library.h>
+// http://librarymanager/All#SparkFun_I2C_Mux
+// https://github.com/sparkfun/SparkFun_I2C_Mux_Arduino_Library
 
 // 8Servo
 #include "M5_UNIT_8SERVO.h"
 
+// Hardware Config
 #define PIN_PA_SDA 32   // for Core2, 26 for M5Stack Basic
 #define PIN_PA_SCL 33   // for Core2, 27 for M5Stack Basic
 
-#define NUM_SERVO_UNITS_MAX 2
 #define NUM_SERVOS_PER_UNIT 8
 #define NUM_CHANNELS_PAHUB 6
+#define NUM_SERVO_UNITS_MAX 2
+#define ID_8SERVO_UNIT_0 0      // PA_HUB 0 : 8Servo Unit 0
+#define ID_8SERVO_UNIT_1 1      // PA_HUB 1 : 8Servo Unit 1
+
+// Utility
+#if 0
+typedef enum {
+    DEBUG = 0,
+    INFO,
+    WARN,
+    ERROR,
+    CRITICAL,
+} MessageLevel;
+void logf(MessageLevel level, const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+    
+    switch (level) {
+        case DEBUG:
+            Serial.printf("[DEBUG] ");
+            break;
+        case INFO:
+            Serial.printf("[INFO] ");
+            break;
+        case WARN:
+            Serial.printf("[WARN] ");
+            break;
+        case ERROR:
+            Serial.printf("[ERROR] ");
+            break;
+        case CRITICAL:
+            Serial.printf("[CRITICAL] ");
+            break;
+        default:
+            break;
+    }
+    
+    vprintf(format, args);
+    va_end(args);
+}
+#endif
 
 #define ASSERT(condition, line) \
     if (!(condition)) { \
-        Serial.printf("[ERROR] Assertion failed: %d at L%d", #condition, #line);\
+        Serial.printf("[Error] Assertion failed: %d at L%d", #condition, #line);\
         while(1);\
     }
 
-// Sound
+// Beep Sound
 #define TONE_C5 523.251
 #define TONE_E5 659.255
 #define TONE_G5 783.991
@@ -29,72 +72,41 @@
 #define TONE_G6 (TONE_G5 * 2)
 
 // for debug
-#define MONITOR_ENABLE
+#define MONITOR_MOTION_ENABLE
 // Serial.printf("[Info] %d\n", __LINE__);
 
+// Static Members
 // PA HUB
 QWIICMUX i2cMux;
-
 // 8Servo
-#ifdef MONITOR_ENABLE
+#ifdef MONITOR_MOTION_ENABLE
 M5GFX display;
 M5Canvas canvas(&display);
 #endif
 M5_UNIT_8SERVO unit_8servo[NUM_SERVO_UNITS_MAX];
 
-// application
+// for application
 #define ANGLE_LIST_SIZE 5
-uint8_t angle_list[ANGLE_LIST_SIZE] = {0, 45, 90, 135, 180};// for demo
+uint8_t angle_list[ANGLE_LIST_SIZE] = {0, 45, 90, 135, 180}; // for demo
 uint8_t i_angle = 0;
 
-// --
+// Timer Interrupt
 #define ID_TIMER_MOTION 0
-const uint64_t TIMER_MOTION_INTERBAL_US = 500000; // interval : 500ms
+const uint64_t TIMER_MOTION_INTERVAL_US = 500000; // interval : 500ms
 hw_timer_t *timerMotion = NULL;
+bool motion_trigger = false;
 void IRAM_ATTR onTimerMotion()
 {
+    if(motion_trigger)  // block multiple events
+        return;
+
     int time_ms = millis();
 
-    Serial.println("[Info] onTimerMotion, time_ms:" + String(time_ms));
-
-#if 0
-    for(uint8_t ch_pahub = 0; ch_pahub < NUM_CHANNELS_PAHUB; ch_pahub++) {
-        // set PA_HUB channel
-        i2cMux.setPort(ch_pahub);
-        Serial.printf("[Info] L%d, CH_PaHUB: %d\n", __LINE__, ch_pahub);
-
-        switch(ch_pahub) {
-            case 0: // for 8servo unit 0
-            case 1: // for 8servo unit 1
-                for(uint8_t servo_ch = 0; servo_ch < NUM_SERVOS_PER_UNIT; servo_ch++) {
-                    // set servo angle
-                    unit_8servo[ch_pahub].setServoAngle(servo_ch, angle_list[i_angle]);
-                    // monitor
-                    Serial.printf("[Info] L%d, CH:%d DEG: %d\n", __LINE__, servo_ch, angle_list[i_angle]);
-
-#ifdef MONITOR_ENABLE
-                    // canvas.drawRect(0, servo_ch * 20 + 75, 200, 15, 1);
-                    // canvas.fillRect(0, servo_ch * 20 + 75, map(angle_list[i_angle], 0, 180, 0, 200), 15, 1);
-                    // canvas.setCursor(220, servo_ch * 28 + 10);
-                    // canvas.setTextSize(1);
-                    // canvas.printf("CH:%d DEG: %d\n", servo_ch, angle_list[i_angle]);
-#endif
-                }
-                break;
-            default:
-                break;
-        }
-        i_angle++;
-        i_angle = i_angle % ANGLE_LIST_SIZE;
-
-#ifdef MONITOR_ENABLE
-        // canvas.pushSprite(0, 0);
-#endif
-        // vTaskDelay(50);    // 500
-    }
-#endif
+    Serial.printf("[Info] onTimerMotion, time_ms:%d\n", time_ms);
+    motion_trigger = true;
 }
 
+// user functions
 void beep()
 {
     M5.Speaker.tone(TONE_E5, 200);
@@ -118,13 +130,13 @@ void setup()
     // I2C init
     Wire.begin(PIN_PA_SDA, PIN_PA_SCL);
     if (!i2cMux.begin(0x70, Wire)) {
-        Serial.println("[Error] PaHUB not detected. Freezing...");
+        Serial.printf("[Error] PaHUB not detected. Freezing...");
         ASSERT(0, __LINE__);
     }
-    Serial.println("[Info] PaHUB detected. Begin scanning for I2C devices");
+    Serial.printf("[Info] PaHUB detected. Begin scanning for I2C devices");
 
     // 8Servo init
-#ifdef MONITOR_ENABLE
+#ifdef MONITOR_MOTION_ENABLE
     display.begin();
     canvas.setColorDepth(1);  // mono color
     canvas.setFont(&fonts::efontCN_14);
@@ -147,7 +159,7 @@ void setup()
             case 1: // for 8servo unit 1
                 if (!unit_8servo[ch_pahub].begin(&Wire, PIN_PA_SDA, PIN_PA_SCL, M5_UNIT_8SERVO_DEFAULT_ADDR)) {
                     Serial.printf("[Error] L%d, ch_pahub %d\n", __LINE__, ch_pahub);
-                    Serial.println("[Error] extio Connect Error\n");
+                    Serial.printf("[Error] extio Connect Error\n");
                     M5.Lcd.print("[Error] extio Connect Error\n");
                     ASSERT(0, __LINE__);
                 }
@@ -161,7 +173,7 @@ void setup()
     // timer must start after device init done
     timerMotion = timerBegin(ID_TIMER_MOTION, 80, true);   // divider:80 for 1us count
     timerAttachInterrupt(timerMotion, &onTimerMotion, true);
-    timerAlarmWrite(timerMotion, TIMER_MOTION_INTERBAL_US, true);
+    timerAlarmWrite(timerMotion, TIMER_MOTION_INTERVAL_US, true);
     timerAlarmEnable(timerMotion);
 
     // init done
@@ -177,7 +189,7 @@ void loop()
     M5.update();
 
     // Button Event
-    //   BtnA : stop timerMotion
+    //   BtnA : pause timerMotion
     //   BtnC : restart timerMotion
     if(M5.BtnA.wasPressed()) {
         Serial.printf("[Info] L%d, BtnA was pressed.\n", __LINE__);
@@ -197,49 +209,50 @@ void loop()
     }
 
     // 8SERVO
-#ifdef MONITOR_ENABLE
+#ifdef MONITOR_MOTION_ENABLE
     canvas.fillSprite(0);
     canvas.setTextSize(2);
     canvas.drawString("SERVO CTL MODE", 10, 10);
     // canvas.drawString("FW VERSION: " + String(unit_8servo[].getVersion()), 10, 40);
 #endif
 
-#if 0 // TODO: port to timer interrupt
-    for(uint8_t ch_pahub = 0; ch_pahub < NUM_CHANNELS_PAHUB; ch_pahub++) {
-        // set PA_HUB channel
-        i2cMux.setPort(ch_pahub);
-        Serial.printf("[Info] L%d, CH_PaHUB: %d\n", __LINE__, ch_pahub);
+    if(motion_trigger) {
+        for(uint8_t ch_pahub = 0; ch_pahub < NUM_CHANNELS_PAHUB; ch_pahub++) {
+            // set PA_HUB channel
+            i2cMux.setPort(ch_pahub);
+            Serial.printf("[Info] L%d, CH_PaHUB: %d\n", __LINE__, ch_pahub);
 
-        switch(ch_pahub) {
-            case 0: // for 8servo unit 0
-            case 1: // for 8servo unit 1
-                for(uint8_t servo_ch = 0; servo_ch < NUM_SERVOS_PER_UNIT; servo_ch++) {
-                    // set servo angle
-                    unit_8servo[ch_pahub].setServoAngle(servo_ch, angle_list[i_angle]);
-                    // monitor
-                    Serial.printf("[Info] L%d, CH:%d DEG: %d\n", __LINE__, servo_ch, angle_list[i_angle]);
+            switch(ch_pahub) {
+                case 0: // for 8servo unit 0
+                case 1: // for 8servo unit 1
+                    for(uint8_t servo_ch = 0; servo_ch < NUM_SERVOS_PER_UNIT; servo_ch++) {
+                        // set servo angle
+                        unit_8servo[ch_pahub].setServoAngle(servo_ch, angle_list[i_angle]);
+                        Serial.printf("[Info] L%d, CH:%d DEG: %d\n", __LINE__, servo_ch, angle_list[i_angle]);
 
-#ifdef MONITOR_ENABLE
-                    canvas.drawRect(0, servo_ch * 20 + 75, 200, 15, 1);
-                    canvas.fillRect(0, servo_ch * 20 + 75, map(angle_list[i_angle], 0, 180, 0, 200), 15, 1);
-                    canvas.setCursor(220, servo_ch * 28 + 10);
-                    canvas.setTextSize(1);
-                    canvas.printf("CH:%d DEG: %d\n", servo_ch, angle_list[i_angle]);
-#endif
-                }
-                break;
-            default:
-                break;
+    #ifdef MONITOR_MOTION_ENABLE
+                        canvas.drawRect(0, servo_ch * 20 + 75, 200, 15, 1);
+                        canvas.fillRect(0, servo_ch * 20 + 75, map(angle_list[i_angle], 0, 180, 0, 200), 15, 1);
+                        canvas.setCursor(220, servo_ch * 28 + 10);
+                        canvas.setTextSize(1);
+                        canvas.printf("CH:%d DEG: %d\n", servo_ch, angle_list[i_angle]);
+    #endif
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+    #ifdef MONITOR_MOTION_ENABLE
+            canvas.pushSprite(0, 0);
+    #endif
+            // vTaskDelay(5);
         }
         i_angle++;
         i_angle = i_angle % ANGLE_LIST_SIZE;
 
-#ifdef MONITOR_ENABLE
-        canvas.pushSprite(0, 0);
-#endif
-        vTaskDelay(50);    // 500
+        motion_trigger = false; // reset trigger
     }
-#endif
 
     vTaskDelay(50);
 }
