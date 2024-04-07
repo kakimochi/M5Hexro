7b66858a40b24ee4c03d23cb6904661e2e97dba9
kmokmops <94213372+kmokmops@users.noreply.github.com>
2024-04-07 09:16:24 +0900

    - Cleanup code

--- M5Hexro/src/main.cpp
+++ M5Hexro/src/main.cpp
@@ -1,15 +1,13 @@
 #include <M5Unified.h>
-// #include <M5Stack.h>
+#include <M5GFX.h>
 
 #define DEBUG_PA
 
+// PA HUB
 #include <Wire.h>
-
-#ifdef DEBUG_PA
 #include <SparkFun_I2C_Mux_Arduino_Library.h> // http://librarymanager/All#SparkFun_I2C_Mux https://github.com/sparkfun/SparkFun_I2C_Mux_Arduino_Library
-#endif
 
-#include <M5GFX.h>
+// 8Servo
 #include "M5_UNIT_8SERVO.h"
 
 #define PIN_PA_SDA 32   // for Core2, 26 for M5Stack Basic
@@ -19,10 +17,8 @@
 #define NUM_SERVOS_PER_UNIT 8
 #define NUM_CHANNELS_PAHUB 6
 
-#ifdef DEBUG_PA
 // PA HUB
 QWIICMUX i2cMux;
-#endif
 
 // 8SERVO
 M5GFX display;
@@ -30,13 +26,13 @@ M5Canvas canvas(&display);
 M5_UNIT_8SERVO unit_8servo[NUM_SERVO_UNITS_MAX];
 char info[50];
 
+// application
 #define ANGLE_LIST_SIZE 5
 uint8_t angle_list[ANGLE_LIST_SIZE] = {0, 45, 90, 135, 180};// for demo
 uint8_t i_angle = 0;
 
-
-// Error
-// uint8_t error_connection = 0;
+// for debug
+// Serial.printf("[Info] %d\n", __LINE__);
 
 // ----
 void setup()
@@ -44,10 +40,8 @@ void setup()
     auto cfg = M5.config();
     M5.begin(cfg);
     M5.Power.begin();
-
-#ifdef DEBUG_PA
-    // Serial.begin(115200);
     delay(100);
+
     Wire.begin(PIN_PA_SDA, PIN_PA_SCL);
     if (i2cMux.begin(0x70, Wire) == false)
     {
@@ -56,7 +50,6 @@ void setup()
             ;
     }
     Serial.println("[Info] PaHUB detected. Begin scanning for I2C devices");
-#endif
 
     // 8SERVO
     display.begin();
@@ -79,11 +72,8 @@ void setup()
         }
 
         switch(ch_pahub) {
-            case 0: // for 8servo 0
-                // connect the module that connected PA_HUB
-                // Wire.beginTransmission(M5_UNIT_8SERVO_DEFAULT_ADDR);
-                // Serial.printf("[Info] L%d, ch_pahub %d\n", __LINE__, ch_pahub);
-
+            case 0: // for 8servo unit 0
+            case 1: // for 8servo unit 1
                 while (!unit_8servo[ch_pahub].begin(&Wire, PIN_PA_SDA, PIN_PA_SCL, M5_UNIT_8SERVO_DEFAULT_ADDR)) {
                     if(error_count < ERROR_COUNT_MAX) {
                         Serial.printf("[Error] L%d, ch_pahub %d\n", __LINE__, ch_pahub);
@@ -94,18 +84,6 @@ void setup()
                     delay(100);
                 }
                 unit_8servo[ch_pahub].setAllPinMode(SERVO_CTL_MODE);
-                // Serial.printf("[Info] %d\n", __LINE__);
-
-                // close connection
-                // error_connection = Wire.endTransmission();
-                // if (error_connection == 0) {
-                //     Serial.printf("[Info] PaHUB CH:%d, 0X%X\n", ch_pahub, M5_UNIT_8SERVO_DEFAULT_ADDR);
-                // } else {
-                //     Serial.printf("[Error] PaHUB CH:%d, 0X%X\n", ch_pahub, M5_UNIT_8SERVO_DEFAULT_ADDR);
-                // }
-                break;
-            case 1: // for 8servo 1
-                // TODO
                 break;
             default:
                 break;
@@ -122,34 +100,12 @@ void loop()
 {
     M5.update();
 
-#ifdef DEBUG_PA
-    // PA HUB
-    // for (uint8_t channel = 0; channel < 8; channel++)
-    // {
-    //     Serial.printf("CH%d : ", channel);
-    //     i2cMux.setPort(channel);
-    //     Serial.print("I2C device = ");
-    //     for (uint8_t address = 0x01; address < 0x7F; address++)
-    //     {
-    //         Wire.beginTransmission(address);
-    //         uint8_t returnCode = Wire.endTransmission();
-    //         if (returnCode == 0)
-    //         {
-    //             Serial.printf("0X%X ", address);
-    //         }
-    //     }
-    //     Serial.println();
-    // }
-    // delay(1000);
-#endif
-
     // 8SERVO
     canvas.fillSprite(0);
     canvas.setTextSize(2);
     canvas.drawString("SERVO CTL MODE", 10, 10);
     // canvas.drawString("FW VERSION: " + String(unit_8servo[].getVersion()), 10, 40);
 
-    // error_connection = 0;
     for(uint8_t ch_pahub = 0; ch_pahub < NUM_CHANNELS_PAHUB; ch_pahub++)
     {
         // set PA_HUB channel
@@ -157,9 +113,8 @@ void loop()
         Serial.printf("[Info] L%d, CH_PaHUB: %d\n", __LINE__, ch_pahub);
 
         switch(ch_pahub) {
-            case 0: // for 8servo 0
-                // open connection
-                // Wire.beginTransmission(M5_UNIT_8SERVO_DEFAULT_ADDR);
+            case 0: // for 8servo unit 0
+            case 1: // for 8servo unit 1
                 for(uint8_t servo_ch = 0; servo_ch < NUM_SERVOS_PER_UNIT; servo_ch++) {
                     // set servo angle
                     unit_8servo[ch_pahub].setServoAngle(servo_ch, angle_list[i_angle]);
@@ -172,23 +127,6 @@ void loop()
                     canvas.setTextSize(1);
                     canvas.printf("CH:%d DEG: %d\n", servo_ch, angle_list[i_angle]);
                 }
-                // close connection
-                // error_connection = Wire.endTransmission();
-                // if (error_connection == 0)
-                // {
-                //     Serial.printf("[Info] PaHUB CH:%d, 0X%X\n", ch_pahub, M5_UNIT_8SERVO_DEFAULT_ADDR);
-                // }
-                break;
-            case 1: // for 8servo 1
-                // connect the module that connected PA_HUB
-                // Wire.beginTransmission(M5_UNIT_8SERVO_DEFAULT_ADDR);
-
-                // // close connection
-                // error_connection = Wire.endTransmission();
-                // if (error_connection == 0)
-                // {
-                //     Serial.printf("[Info] PaHUB CH:%d, 0X%X\n", ch_pahub, M5_UNIT_8SERVO_DEFAULT_ADDR);
-                // }
                 break;
             default:
                 break;
@@ -199,17 +137,4 @@ void loop()
         canvas.pushSprite(0, 0);
         vTaskDelay(50);    // 500
     }
-
-    // delay(10);  // 100
 }
-
-// Mux detected
-// Begin scanning for I2C devices
-// CH0 : I2C device = 0X25 0X70
-// CH1 : I2C device = 0X25 0X70 
-// CH2 : I2C device = 0X70 
-// CH3 : I2C device = 0X70 
-// CH4 : I2C device = 0X70 
-// CH5 : I2C device = 0X70 
-// CH6 : I2C device = 0X70 
-// CH7 : I2C device = 0X70 
